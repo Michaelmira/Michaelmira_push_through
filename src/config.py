40308e0ffdaf6c0d9ef1005e0a34a51762ab165c
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-27 01:46:16 +0000

    create config.py and add classes and functions to set game

--- src/config.py
+++ src/config.py
@@ -1,2 +1,35 @@
 
 class config():
+    def __init__(self):
+        self.boards = None
+        self.win_condition = None
+        self.player_colors = None
+        self.game_width = None
+        self.game_height = None
+        self.total_pushes = None
+        self.total_turns_per_player = None
+        self.turn_time_limit = None
+
+    def set_boards(self, boards):
+        self.boards = boards
+
+    def set_win_condition(self, win_condition):
+        self.win_condition = win_condition
+
+    def set_player_colors(self, player_colors):
+        self.player_colors = player_colors
+
+    def set_game_width(self, game_width):
+        self.game_width = game_width
+
+    def set_game_height(self, game_height):
+        self.game_height = game_height
+
+    def set_total_pushes(self, total_pushes):
+        self.total_pushes = total_pushes
+
+    def set_total_turns_per_player(self, total_turns_per_player):
+        self.total_turns_per_player = total_turns_per_player
+
+    def set_turn_time_limit(self, turn_time_limit):
+        self.turn_time_limit = turn_time_limit
