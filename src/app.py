68287dd74d4437cf22b273a62d7c990d8813ee2e
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-04-16 20:37:44 +0000

    Tidy up codespace, Save for PR

--- src/app.py
+++ src/app.py
@@ -6,20 +6,19 @@ from board import *
 cube = Cube(DEFAULT_GAME_HEIGHT, DEFAULT_GAME_WIDTH, num_boards=DEFAULT_NUM_BOARDS)
 
 #Grab a cell and make sure it looks good
-# first_board = cube.boards[0]
+first_board = cube.boards[0]
+some_cell = first_board.get_cell(2, 3)
+print(some_cell)
 
-# some_cell = first_board.get_cell(2, 3)
-# print(some_cell)
+first_board.print_ascii_game_board()
+
+new_game_config = {}
+new_game = Game(new_game_config)
+
+new_game.play_game()
 
 
-# first_board.print_ascii_game_board()
 game_board = GameBoard(10, 7, 4)
 
 game_board.print_ascii_game_boards()
-
-
-# new_game_config = {}
-# new_game = Game(new_game_config)
-
-# new_game.play_game()
 
